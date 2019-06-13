0f917db5996eb98fdc340e65cd67b76342d27510
Rails Udemy Course <cristian.ernszt@softvision.ro>
2019-06-13 14:09:14 +0000

    Added edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
     def create
         @article = Article.new(article_params)
         if @article.save
@@ -13,13 +17,23 @@ class ArticlesController < ApplicationController
         end
     end
     
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+    
     def show
         @article = Article.find(params[:id])
     end
     
     private
-    def article_params
-       params.require(:article).permit(:title, :description) 
-    end
+        def article_params
+           params.require(:article).permit(:title, :description) 
+        end
     
 end
